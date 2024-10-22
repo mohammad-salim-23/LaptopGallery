4ce6bd6d82a61dbea11e376a27a62b6660cf4411
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2024-10-23 02:12:58 +0600

    Project Setup & Create Branch

---
+++ src/Layout/Main/Main.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "../../Components/Shared/NavBar/NavBar";
+import Footer from "../../Components/Shared/Footer/Footer";
+
+const Main = () => {
+    return (
+        <div className="">
+
+            <NavBar></NavBar>
+
+            <Outlet></Outlet>
+
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
