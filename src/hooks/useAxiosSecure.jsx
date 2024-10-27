7aeadc5e5855b8a7825092ae74d1b7737754eff0
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-10-27 17:41:33 +0600

    useAxiosSecure component add

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../Components/Shared/AuthContext/AuthProvider";
+
+const axiosSecure = axios.create({
+    baseURL:"http://localhost:5000",
+});
+const useAxiosSecure=()=>{
+    const navigate = useNavigate();
+    const {logOut} = useContext(AuthContext);
+    // request interceptor to add authorization header for every secure call to the api
+    axiosSecure.interceptors.request.use(
+        function(config){
+            const token = localStorage.getItem("access-token");
+            console.log("request stopped by interceptors");
+            config.headers.authorization=`Bearer ${token}`;
+            return config;
+        },
+        (error)=>{
+             // Do something with request error
+      return Promise.reject(error);
+        }
+    );
+     //  interceptors 401 and 403 status
+     axiosSecure.interceptors.response.use(
+        function(response){
+            return response;
+        },
+        async(error)=>{
+            const status = error.response.status;
+            console.log("status error in the interceptors", error);
+            // for 401 or 403 logOut the user and move the user to the login page
+            if(status==401 || status==403){
+                await logOut();
+                navigate("signin");
+
+            }
+            return Promise.reject(error);
+        }
+     );
+     return axiosSecure;
+}
+export default useAxiosSecure();
