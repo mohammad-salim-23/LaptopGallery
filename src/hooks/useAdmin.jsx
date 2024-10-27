b79ac68661727f3e3aa426bb8158e9d244c52863
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-10-27 18:08:32 +0600

    SignUp page create

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -1,12 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
 import { AuthContext } from "../Components/Shared/AuthContext/AuthProvider"
+import useAxiosSecure from "./useAxiosSecure";
 
 const useAdmin=()=>{
     const {user,loading} = useContext(AuthContext);
-    
-    return(
-        <div>
-
-        </div>
-    )
+    const axiosSecure = useAxiosSecure();
+    const {data:isAdmin,isPending:isAdminLoading}=useQuery({
+        queryKey:[user?.email,'isAdmin'],
+        enabled:!loading,
+        queryFn:async()=>{
+            const res = await axiosSecure.get(`/users/admin/${user.email}`)
+            console.log(res.data);
+            return res.data?.admin;
+        }
+    })
+    return [isAdmin,isAdminLoading];
 }
 export default useAdmin;
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import "./index.css";
 import {RouterProvider} from "react-router-dom";
 import { router } from './Router/Routers';
 import AuthProvider from './Components/Shared/AuthContext/AuthProvider';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+    </QueryClientProvider>
     </AuthProvider>
   </StrictMode>,
 )
