20aed6e2f6c9ae1f44a2c8dd97f14236359fe838
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2024-10-29 00:26:57 +0600

    Banner res. & Find addede , category section added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import './styles.css';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import { Link } from 'react-router-dom';
-
+import { IoMdSearch } from 'react-icons/io';
 
 const slidesData = [
     {
@@ -18,81 +17,58 @@ const slidesData = [
     {
         image: "https://img.freepik.com/free-photo/female-hand-typing-keyboard-laptop_1150-15742.jpg?t=st=1729861804~exp=1729865404~hmac=21c5ffd06bb3982c69d5a7b4d17a4b39dc67f7b80a6b37c711d5d019d74dc965&w=1380",
     }
-
-
 ];
 
 const Banner = () => {
     return (
-        <div className="container mx-auto">
-            {/*  */}
-            <div className="flex mt-10">
+        <div className="container mx-auto px-4">
+            <div className="flex flex-col lg:flex-row mt-10 space-y-6 lg:space-y-0 lg:space-x-6">
+                <div className="lg:w-1/4 w-full">
+                    <div className="bg-yellow-200 pt-6   rounded-md shadow-md mb-6 lg:mb-0">
+                        <h1 className="font-semibold text-center text-lg text-black">Compare Products</h1>
+                        <p className="mt-1 text-center text-gray-600 text-sm">Choose Two Products to Compare</p>
 
-                <div className="mr-6 w-[25%]">
-
-                    <div className='h-[53%] bg-[#FFE8A1] pt-6'>
-                        <h1 className='font-semibold text-center'>Compare Products</h1>
-                        <p className='mt-1 opacity-60 text-center'>Choose Two Products to Compare</p>
-
-                        <div className=' p-4 rounded-md'>
-                            {/* Search 1 */}
-                            <div className='relative items-center mb-4 ml-4 cursor-pointer'>
+                        <div className="p-4">
+                            <div className="relative mb-4">
                                 <input
-                                    type='text'
-                                    placeholder='Search and Select Product'
-                                    className='w-72 p-3 pl-4 rounded-md shadow-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white'
+                                    type="text"
+                                    placeholder="Search Product..."
+                                    className="w-full p-3 rounded-md border bg-white border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                                 />
-                                <span className='absolute right-3 top-3 text-gray-400 mr-8'>
-                                    <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' viewBox='0 0 20 20' fill='currentColor'>
-                                        <path
-                                            fillRule='evenodd'
-                                            d='M12.9 14.32a8 8 0 111.414-1.414l3.025 3.025a1 1 0 11-1.414 1.414l-3.025-3.025zM8 14A6 6 0 108 2a6 6 0 000 12z'
-                                            clipRule='evenodd'
-                                        />
-                                    </svg>
+                                <span className="absolute right-3 top-3 text-gray-400">
+                                <IoMdSearch className='text-3xl' />
                                 </span>
                             </div>
 
-
-                            <div className='relative items-center mb-4 ml-4 cursor-pointer'>
+                            <div className="relative mb-4">
                                 <input
-                                    type='text'
-                                    placeholder='Search and Select Product'
-                                    className='w-72 p-3 pl-4 rounded-md shadow-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white'
+                                    type="text"
+                                    placeholder="Search Product..."
+                                    className="w-full p-3 rounded-md border bg-white border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
                                 />
-                                <span className='absolute right-3 top-3 text-gray-400 mr-8'>
-                                    <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5' viewBox='0 0 20 20' fill='currentColor'>
-                                        <path
-                                            fillRule='evenodd'
-                                            d='M12.9 14.32a8 8 0 111.414-1.414l3.025 3.025a1 1 0 11-1.414 1.414l-3.025-3.025zM8 14A6 6 0 108 2a6 6 0 000 12z'
-                                            clipRule='evenodd'
-                                        />
-                                    </svg>
+                                <span className="absolute right-3 top-3 text-gray-400">
+                                <IoMdSearch className='text-3xl' />
                                 </span>
                             </div>
 
-
-
-                            {/* Button */}
-                            <button className='w-72 ml-4 p-3 bg-white text-blue-600 font-semibold border border-blue-600 rounded-md hover:bg-blue-600 hover:text-white transition-colors'>
+                            <button className="w-full p-3 font-semibold text-blue-600 border border-blue-600 rounded-md hover:bg-blue-600 hover:text-white transition-colors my-3 md:my-0">
                                 View Comparison
                             </button>
                         </div>
-
                     </div>
 
-                    <div className=' mt-6 h-[47%]'>
+                    <div className="h-1/2 lg:my-6 md:my-4 my-2">
                         <Link>
-                            <img src={"https://www.startech.com.bd/image/catalog/home/banner/small/Shape-Your-Career-with-Us.png1.webp"} alt="" className='  w-full' />
+                            <img
+                                src="https://www.startech.com.bd/image/catalog/home/banner/small/Shape-Your-Career-with-Us.png1.webp"
+                                alt=""
+                                className="w-full rounded-md shadow-md"
+                            />
                         </Link>
                     </div>
-
                 </div>
 
-
-
-
-                <div className=" w-[75%]">
+                <div className="lg:w-3/4 w-full">
                     <Swiper
                         slidesPerView={1}
                         spaceBetween={30}
@@ -106,17 +82,16 @@ const Banner = () => {
                         }}
                         navigation={true}
                         modules={[Autoplay, Pagination, Navigation]}
-                        className="mySwiper"
+                        className="rounded-md shadow-md"
                     >
                         {slidesData.map((slide, index) => (
                             <SwiperSlide key={index}>
                                 <div
-                                    className="w-full bg-center bg-cover h-[34rem]  object-cover "
+                                    className="h-72 sm:h-80 md:h-96 lg:h-[34rem] bg-cover bg-center rounded-md"
                                     style={{
                                         backgroundImage: `url("${slide.image}")`,
                                     }}
-                                >
-                                </div>
+                                />
                             </SwiperSlide>
                         ))}
                     </Swiper>
---
+++ src/Pages/Home/Category/Category.jsx
@@ -0,0 +1,53 @@
+import Marquee from "react-fast-marquee";
+
+
+const Category = () => {
+    const categories = [
+        {
+            "id": 1,
+            "title": "Laptop",
+            "icon": "📷"
+        },
+        {
+            "id": 2,
+            "title": "Laptop Accessories",
+            "icon": "📷"
+        },
+        {
+            "id": 3,
+            "title": "Mobile ",
+            "icon": "📷"
+        }, {
+            "id": 4,
+            "title": "Mobile Accessories",
+            "icon": "📷"
+        }
+    ];
+
+    return (
+        <div className="mx-auto container">
+            <div className="text-center my-10">
+                <h1 className="text-3xl font-bold">Featured Category</h1>
+                <p className="text-xl text-black text-opacity-50 my-2">
+                    Get Your Desired Product from Featured Category!
+                </p>
+            </div>
+
+            <Marquee pauseOnHover={true}>
+                <div className="flex justify-center space-x-7">
+                    {categories.map(category => (
+                        <div
+                            key={category.id}
+                            className="flex flex-col items-center p-6 bg-slate-300 hover:bg-slate-400 cursor-pointer rounded-lg shadow-md w-56"
+                        >
+                            <div className="text-4xl mb-4">{category.icon}</div>
+                            <p className="text-lg font-medium">{category.title}</p>
+                        </div>
+                    ))}
+                </div>
+            </Marquee>
+        </div>
+    );
+};
+
+export default Category;
